58c6a6ae5d6dc6526b42a7d471da3124c80c6e1f
David Bray <bray@bu.edu>
2015-11-27 13:19:05 -0500

    Update get_gene_signature.py

--- get_gene_signature.py
+++ get_gene_signature.py
@@ -6,7 +6,7 @@ Created on Wed Nov 18 20:31:41 2015
 """
 import itertools
 
-def get_gene_sig(sample_cluster_file, norm_expr_file, g):
+def get_gene_sig(sample_cluster_file, gene_file, norm_expr_file, g):
     """ gets gene signature from a sample cluster """
     
     # initialize variables needed from the sample cluster file
@@ -29,10 +29,28 @@ def get_gene_sig(sample_cluster_file, norm_expr_file, g):
             samples.append(line_parts[0])
             sample_clusters.append(line_parts[4])
             
+    # initialize a variable to conveniently hold the number of clusters
+    cluster_number = len(set(sample_clusters))
+            
     # initialize variables needed from the normalized expression matrix
     expr_samples = []   # total of m samples
     genes = []          # total of n genes
     expr_matrix = []    # n genes x i (subset of m) matrix
+    
+    # open the DGE list of genes used to cluster
+    with open(gene_file, 'r') as f:
+       line = f.readline() # read the header
+       while True:
+           # read the current line
+           line = f.readline()
+           if not line:
+               break # break at the end of the file
+           
+           # split the line into its parts
+           line_parts = line.split()
+           
+           # store the gene on the line
+           genes.append(line_parts[1])
     
     # open the normalized expression input file
     with open(norm_expr_file, 'r') as f:
@@ -57,7 +75,13 @@ def get_gene_sig(sample_cluster_file, norm_expr_file, g):
             line_parts = line.split()
             
             # store the gene name from the first part
-            genes.append(line_parts[0].replace('"', ''))
+            gene_name = line_parts[0].replace('"', '')
+            
+            # SKIP GENE IF IT IS NOT USED IN THE CLUSTERING
+            if gene_name not in genes:
+                continue
+            
+            # if the gene is used in the clustering
             line_parts = line_parts[1:] # skip the gene name at the beginning
             
             # list comprehension to get expression values from relevant samples
@@ -71,8 +95,7 @@ def get_gene_sig(sample_cluster_file, norm_expr_file, g):
         print("number of genes: " + str(len(expr_matrix)))
 
     # initialize gene signature variables needed
-    gene_sigs = []
-    gene_freq_per_cluster = [{} for c in range(len(set(sample_clusters)))] # defines dictionaries (indexed by cluster number - 1)
+    gene_freq_per_cluster = [{} for c in range(cluster_number)] # defines dictionaries (indexed by cluster number - 1)
     cluster_dict = {samples[i]: int(sample_clusters[i]) for i in range(len(samples))}
         
     # crawl through the expression matrix one sample (column) at a time
@@ -82,30 +105,45 @@ def get_gene_sig(sample_cluster_file, norm_expr_file, g):
             gene_expr_per_sample[genes[n]] = expr_matrix[n][i]
         c = cluster_dict[samples[i]] - 1 # get cluster assignment of current sample
         top_genes = ((gene, gene_expr_per_sample) for gene in sorted(gene_expr_per_sample, key=gene_expr_per_sample.get, reverse = True))
-        for gene, expr in itertools.islice(top_genes, g):
+        for gene, expr in itertools.islice(top_genes, g): # get how many times a gene appears in the top 200
             gene_freq_per_cluster[c][gene] = gene_freq_per_cluster[c].get(gene, 0) + 1
     
-    # determine gene signatures using the top g genes throughout each cluster
-    for c in range(len(set(sample_clusters))):
-        top_cluster_genes = ((gene, gene_freq_per_cluster[c][gene]) for gene in sorted(gene_freq_per_cluster[c], key=gene_freq_per_cluster[c].get, reverse = True))
-        gene_list = []
-        print("--- NEW CLUSTER ---")
-        for gene, freq in itertools.islice(top_cluster_genes, g):
-            print(gene + ": " + str(freq))
-            gene_list.append(gene)
-        gene_sigs.append(gene_list)
+    # sort the most frequence genes in each cluster
+    top_cluster_genes = []  # initialize a list of tuples to hold the gene, frequency pairs
+    for c in range(cluster_number):
+        top_genes = [(gene, gene_freq_per_cluster[c][gene]) for gene in sorted(gene_freq_per_cluster[c], key=gene_freq_per_cluster[c].get, reverse = True)]
+        top_cluster_genes.append([gene_tuple[0] for gene_tuple in top_genes])
     
+    # determine the unique gene signatures based on the top cluster genes previously determined
+    gene_sigs = []
+    for c in range(cluster_number):
+        gene_sigs.append([])
+    used_gene_set = set()
+    for x in range(g):
+        for c in range(cluster_number):
+            if top_cluster_genes[c][x] not in used_gene_set:
+                used_gene_set.add(top_cluster_genes[c][x])
+                gene_sigs[c].append(top_cluster_genes[c][x])
+                
+    # determine the minimum number of genes in a gene signature
+    min_number = 10000
+    for c in range(cluster_number):
+        if len(gene_sigs[c]) < min_number:
+            min_number = len(gene_sigs[c])
+                
     # return the gene signatures (1 for each cluster)
+    for c in range(cluster_number):
+        gene_sigs[c] = gene_sigs[c][:min_number] # keeps number symmetric
     return gene_sigs
 
 
 def print_sigs_to_file(gene_sigs, dir_path, g):
-    o = open(dir_path + "sample_cluster_gene_sigs.txt", 'w')
+    o = open(dir_path + "sample_cluster_gene_sigs_TEST.txt", 'w')
     header = ""   
     for i in range(len(gene_sigs)):
         header += str(i+1) + '\t'
     o.write(header.rstrip() + '\n')
-    for n in range(g):
+    for n in range(len(gene_sigs[0])):
         line = ""
         for c in range(len(gene_sigs)):
             line += (gene_sigs[c][n] + '\t')
@@ -118,9 +156,10 @@ def main():
     dir_path = "C:/Users/dbray/Dropbox/Boston University/Fall 2015/Challenge Project/TrainingSet/"
     sample_cluster_file = dir_path + "sampK3_clusters.txt"
     norm_expr_file = dir_path + "NormalizedExpMat.txt"
+    gene_file = dir_path + "genes4clustering.txt"
     g = 200 # desired size of gene signatures
     
-    gene_sigs = get_gene_sig(sample_cluster_file, norm_expr_file, g)
+    gene_sigs = get_gene_sig(sample_cluster_file, gene_file, norm_expr_file, g)
     print_sigs_to_file(gene_sigs, dir_path, g)
 
 if __name__ == "__main__":
